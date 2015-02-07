fb58cca45bd0787d94d2176f009c45b08d8f2030
zhs007 <sssxueren@gmail.com>
2015-02-07 17:31:22 +0800

    0.5.11

--- revertproj.js
+++ revertproj.js
@@ -6,11 +6,11 @@ var console = require('console');
 var path = require('path');
 var fileutils = require('heyutils').fileutils;
 var csv2obj = require('heyutils').csv2obj;
-var cocos2dxutils = require('../cocos2dxutils');
+var cocos2dxutils = require('./cocos2dxutils');
 
 function proc(projname) {
     var projconfig = csv2obj.loadCSVSync(projname);
-    
+
     var destdir = projconfig[0].destpath;
 
     cocos2dxutils.revertCocos2dxHelloWorldCpp_cc3(destdir);
